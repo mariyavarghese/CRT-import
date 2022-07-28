972792d5d20eba599283b080ab7ab1fdab0c0fc2
ananthusvn <100422811+ananthusvn@users.noreply.github.com>
2022-07-28 18:19:11 +0530

    layout

---
+++ app/src/main/java/com/example/crt/BluetoothActivity.java
@@ -0,0 +1,41 @@
+package com.example.crt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+
+public class BluetoothActivity extends AppCompatActivity {
+    ListView l;
+    ListView k;
+    String devices[]
+            = { "Paired Devices", "Device1","Device1","Device1","Device1","Device1" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+
+        getSupportActionBar().setTitle("Configuration");
+
+         l = findViewById(R.id.listAvail);
+        ArrayAdapter<String> arr;
+        arr
+                = new ArrayAdapter<String>(
+                this,
+                R.layout.support_simple_spinner_dropdown_item,
+                devices);
+        l.setAdapter(arr);
+
+        k = findViewById(R.id.list);
+        ArrayAdapter<String> arra;
+        arra
+                = new ArrayAdapter<String>(
+                this,
+                R.layout.support_simple_spinner_dropdown_item,
+                devices);
+        l.setAdapter(arr);
+    }
+}
--- app/src/main/java/com/example/crt/BtlistActivity.java
+++ app/src/main/java/com/example/crt/BtlistActivity.java
@@ -2,16 +2,13 @@ package com.example.crt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BtlistActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent k = new Intent(this, BluetoothActivity.class);
-        startActivity(k);
+        setContentView(R.layout.activity_btlist);
     }
 }
