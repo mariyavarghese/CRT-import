c1d5b0e5869365cddaecc01390e75cadc00a211a
ananthusvn <100422811+ananthusvn@users.noreply.github.com>
2022-08-03 23:39:47 +0530

    frontend

--- app/src/main/java/com/example/crt/MainActivity.java
+++ app/src/main/java/com/example/crt/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent k = new Intent(this, BluetoothActivity.class);
+        Intent k = new Intent(this, DetailActivity.class);
         startActivity(k);
     }
 }
