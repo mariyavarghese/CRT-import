608d2b1d570aabf4a2b0f44ea86222aaa52c9cd3
ananthusvn <100422811+ananthusvn@users.noreply.github.com>
2022-08-05 01:11:34 +0530

    added backend

--- app/src/main/java/com/example/crt/BluetoothActivity.java
+++ app/src/main/java/com/example/crt/BluetoothActivity.java
@@ -5,8 +5,13 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
@@ -14,9 +19,18 @@ import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.UUID;
 
 
 public class BluetoothActivity extends AppCompatActivity {
@@ -26,9 +40,7 @@ public class BluetoothActivity extends AppCompatActivity {
 
     ListView l;
     ListView k;
-    String devices[]
-            = {"Paired Devices", "Device1", "Device1", "Device1", "Device1", "Device1"};
-
+    String devices[] =new String[]{ "Dev1","dev2" };;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,26 +52,60 @@ public class BluetoothActivity extends AppCompatActivity {
         BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
 
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
         //
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+
+            promptbt();
+        } else {
+            try {
+                searchbt();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
 
 
 
+//
 
-         l = findViewById(R.id.listAvail);
+    }
+    public void searchbt() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+        //
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d("bt bt",deviceName);
+                Log.d("bt bt",deviceHardwareAddress);
+                Method getUuidsMethod = BluetoothAdapter.class.getDeclaredMethod("getUuids", null);
+
+                ParcelUuid[] uuids = (ParcelUuid[]) getUuidsMethod.invoke(mBluetoothAdapter, null);
+
+                for (ParcelUuid uuid: uuids) {
+                    Log.d("TAG", "UUID: " + uuid.getUuid().toString());
+                }
+            }
+        }
+        String deviceHardwareAddress="00:1E:42:72:48:3F";
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothDevice btDevice = btAdapter.getRemoteDevice(deviceHardwareAddress);
+        ConnectThread connectThread = new ConnectThread(btDevice);
+        connectThread.start();
+        l = findViewById(R.id.listAvail);
         ArrayAdapter<String> arr;
         arr
                 = new ArrayAdapter<String>(
@@ -67,6 +113,9 @@ public class BluetoothActivity extends AppCompatActivity {
                 R.layout.support_simple_spinner_dropdown_item,
                 devices);
         l.setAdapter(arr);
+
+
+
 
         k = findViewById(R.id.list);
         ArrayAdapter<String> arra;
@@ -76,5 +125,175 @@ public class BluetoothActivity extends AppCompatActivity {
                 R.layout.support_simple_spinner_dropdown_item,
                 devices);
         k.setAdapter(arr);
+
+    }
+
+    protected void onActivityResult (int requestCode,
+                                     int resultCode,
+                                     Intent data) {
+//        Log.d("bt r", String.valueOf(requestCode));
+        super.onActivityResult(requestCode, resultCode, data);
+if(resultCode==-1){
+    try {
+        searchbt();
+    } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+    } catch (InvocationTargetException e) {
+        e.printStackTrace();
+    } catch (IllegalAccessException e) {
+        e.printStackTrace();
     }
 }
+else {
+    promptbt();
+}
+    }
+
+    public void scanbt(View v) {
+        Log.d("bt s", "inside1");
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        mBluetoothAdapter.startDiscovery();
+        Log.d("bt s", "inside2");
+
+        final BroadcastReceiver mReceiver = new BroadcastReceiver()
+        {
+            @Override
+
+            public void onReceive(Context context, Intent intent)
+            {
+                Log.d("bt s", "inside3");
+
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action))
+                {
+                    Log.d("bt s", "inside");
+                    /*
+                    // Get the bluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Get the “RSSI” to get the signal strength as integer,
+                    // but should be displayed in “dBm” units
+                    int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                    // Create the device object and add it to the arrayList of devices
+                    BluetoothObject bluetoothObject = new BluetoothObject();
+                    bluetoothObject.setBluetooth_name(device.getName());
+                    bluetoothObject.setBluetooth_address(device.getAddress());
+                    bluetoothObject.setBluetooth_state(device.getBondState());
+                    bluetoothObject.setBluetooth_type(device.getType());    // requires API 18 or higher
+                    bluetoothObject.setBluetooth_uuids(device.getUuids());
+                    bluetoothObject.setBluetooth_rssi(rssi);
+                    arrayOfFoundBTDevices.add(bluetoothObject);
+                    // 1. Pass context and data to the custom adapter
+                    FoundBTDevicesAdapter adapter = new FoundBTDevicesAdapter(getApplicationContext(), arrayOfFoundBTDevices);
+                    // 2. setListAdapter
+                    setListAdapter(adapter); */
+                }
+            }
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+    /*
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Log.d("bt s","clicked");
+        mBluetoothAdapter.startDiscovery();
+
+       BroadcastReceiver mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+Log.d("bt s","the");
+                //Finding devices
+                if (BluetoothDevice.ACTION_FOUND.equals(action))
+                {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    Log.d("bt devsc",device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+*/
+
+        //   IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+     //   registerReceiver(receiver, filter);
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            Log.d("bt i","insd2");
+
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+                Log.d("bt sc",deviceName);
+            }
+        }
+    };
+
+    public void promptbt(){
+
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+      //  Log.d("bt","bt off");
+
+    }
+
+    //
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket thisSocket;
+        private final BluetoothDevice thisDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            BluetoothSocket tmp = null;
+            thisDevice = device;
+
+            try {
+                tmp = thisDevice.createRfcommSocketToServiceRecord(UUID.fromString("0000110a-0000-1000-8000-00805f9b34fb"));
+            } catch (IOException e) {
+                Log.e("TEST", "Can't connect to service");
+            }
+            thisSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+       //     btAdapter.cancelDiscovery();
+
+            try {
+                thisSocket.connect();
+                Log.d("TESTING", "Connected to shit");
+            } catch (IOException connectException) {
+                try {
+                    thisSocket.close();
+                } catch (IOException closeException) {
+                    Log.e("TEST", "Can't close socket");
+                }
+                return;
+            }
+
+          BluetoothSocket  btSocket = thisSocket;
+
+        }
+        public void cancel() {
+            try {
+                thisSocket.close();
+            } catch (IOException e) {
+                Log.e("TEST", "Can't close socket");
+            }
+        }
+    }
+    //
+}
+
